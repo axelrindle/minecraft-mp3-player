90a32106a08d95cc835f01a13242c5749504d8ca
cwerl <christopher.werl1@gmail.com>
2022-03-28 21:57:02 +0200

    added mp3 player block and item + textures

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,21 +1,21 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final String MODID = "mp3_player";
+    public static final Mp3Player BLOCK_MP3_PLAYER = new Mp3Player(FabricBlockSettings.of(Material.WOOD).strength(1.0f));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+        Registry.register(Registry.BLOCK, new Identifier(MODID, "mp3_player"), BLOCK_MP3_PLAYER);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "mp3_player"), new BlockItem(BLOCK_MP3_PLAYER, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
 	}
 }
---
+++ src/main/java/net/fabricmc/example/Mp3Player.java
@@ -0,0 +1,27 @@
+package net.fabricmc.example;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class Mp3Player extends Block {
+
+    public Mp3Player(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(!world.isClient) {
+            player.sendMessage(new LiteralText("turnup"), false);
+        }
+
+        return ActionResult.SUCCESS;
+    }
+}
